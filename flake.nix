46ab3623a0084df6ed7b21347230d2a1867203f9
Ollie Relph <ollie@relph.me>
2023-10-19 21:51:19 +0100

    Add flake

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "BBB:hex-http";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.nixpkgs-temurin-21.url = "github:Infinidoge/nixpkgs/feat/temurin-bin-21";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, nixpkgs-temurin-21, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      fork = nixpkgs-temurin-21.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell rec {
+        packages = with pkgs; [
+          direnv
+          git
+          fork.temurin-bin-21
+        ];
+      };
+    });
+}
