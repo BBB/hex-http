1a2611534b384e1b3c58635faf8fd1e47c12ade6
Ollie Relph <ollie@relph.me>
2023-11-09 16:25:57 +0000

    Contract test for storage

--- src/test/kotlin/me/relph/domain/adapter/InMemoryUserStorageTest.kt
+++ src/test/kotlin/me/relph/domain/adapter/InMemoryUserStorageTest.kt
@@ -4,14 +4,14 @@ import dev.mrbergin.kotest.result4k.shouldBeFailure
 import dev.mrbergin.kotest.result4k.shouldBeSuccess
 import me.relph.domain.port.User
 import me.relph.domain.port.UserId
+import me.relph.domain.port.UserStorage
 import me.relph.domain.port.UserStorageFailure.NotFound
 import org.junit.jupiter.api.Test
 
 private val roger = User("Roger")
 
-internal class InMemoryUserStorageTest {
-    private val storage = InMemoryUserStorage(mapOf(UserId.of("1") to roger))
-
+interface UserStorageContract {
+    val storage: UserStorage
     @Test
     fun `can get a user by id`() {
         storage.byId(UserId.of("1")) shouldBeSuccess roger
@@ -25,4 +25,8 @@ internal class InMemoryUserStorageTest {
     fun `can get all users`() {
         storage.all() shouldBeSuccess  listOf(roger)
     }
+}
+
+internal class InMemoryUserStorageTest : UserStorageContract {
+    override val storage = InMemoryUserStorage(mapOf(UserId.of("1") to roger))
 }
